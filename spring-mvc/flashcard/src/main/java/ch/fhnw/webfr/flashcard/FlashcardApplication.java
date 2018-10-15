65d00eed141f2c834d9317d6db9dd2997f941aa1
Nicola Cocquio <nicola.cocquio@students.fhnw.ch>
2018-10-15 17:03:06 +0200

    AB9, 10, 10 and UB5

--- spring-mvc/flashcard/src/main/java/ch/fhnw/webfr/flashcard/FlashcardApplication.java
+++ spring-mvc/flashcard/src/main/java/ch/fhnw/webfr/flashcard/FlashcardApplication.java
@@ -2,10 +2,12 @@ package ch.fhnw.webfr.flashcard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class FlashcardApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FlashcardApplication.class, args);
 	}
--- spring-mvc/flashcard/src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
+++ spring-mvc/flashcard/src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
@@ -5,8 +5,10 @@ import ch.fhnw.webfr.flashcard.persistence.QuestionnaireRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -17,22 +19,30 @@ public class QuestionnaireController {
     private QuestionnaireRepository questionnaireRepository;
 
     @GetMapping
-    public String findAll(Model model, @RequestParam(required=false) String form) {
+    public String findAll(Model model, @RequestParam(required=false) String form, @RequestParam(required=false) String message, @RequestParam(required=false) String status) {
         if (null == form) {
             model.addAttribute("questionnaires", questionnaireRepository.findAll());
+            model.addAttribute("message", message);
+            model.addAttribute("status", status);
 
             return "list";
         }
 
         model.addAttribute("questionnaire", new Questionnaire());
+
         return "create";
     }
 
     @PostMapping
-    public String create(Questionnaire questionnaire) {
+    public String create(@Valid Questionnaire questionnaire, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "create";
+        }
+
         questionnaireRepository.insert(questionnaire);
 
-        return "create";
+        return "redirect:/questionnaires?message=Card inserted!&status=success";
     }
 
     @GetMapping(path = "/{id}")
@@ -41,8 +51,53 @@ public class QuestionnaireController {
 
         if (questionnaire.isPresent()) {
             model.addAttribute("questionnaire", questionnaire.get());
+        } else {
+            return "redirect:/questionnaires?message=Card not found!&status=error";
+            //return "404";
         }
 
         return "show";
+    }
+
+    @GetMapping(path = "edit/{id}")
+    public String editById(@PathVariable String id, Model model) {
+        Optional<Questionnaire> questionnaire = questionnaireRepository.findById(id);
+
+        if (questionnaire.isPresent()) {
+            model.addAttribute("questionnaire", questionnaire.get());
+        } else {
+            return "redirect:/questionnaires?message=Card not found!&status=error";
+            //return "404";
+        }
+
+        return "edit";
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String delete(@PathVariable String id, Model model) {
+        Optional<Questionnaire> questionnaire = questionnaireRepository.findById(id);
+
+        if (questionnaire.isPresent()) {
+            questionnaireRepository.delete(questionnaire.get());
+        } else {
+            return "redirect:/questionnaires?message=Card not found!&status=error";
+            //return "404";
+        }
+
+        return "redirect:/questionnaires?message=Card deleted!&status=success";
+    }
+
+    @PutMapping
+    public String editById(@Valid Questionnaire questionnaire, BindingResult result, Model model) {
+        Optional<Questionnaire> oldQuestionnaire = questionnaireRepository.findById(questionnaire.getId());
+
+        if (oldQuestionnaire.isPresent()) {
+            questionnaireRepository.save(questionnaire);
+        } else {
+            return "redirect:/questionnaires?message=Card not found!&status=error";
+            //return "404";
+        }
+
+        return "redirect:/questionnaires?message=Card updated!&status=success";
     }
 }
