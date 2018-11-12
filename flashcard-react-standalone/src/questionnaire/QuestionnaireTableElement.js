816c9d9457f54a78c280b9423d0d0a0462fcdee1
Nicola Cocquio <nicola.cocquio@students.fhnw.ch>
2018-11-12 17:57:14 +0100

    AB17

--- flashcard-react-standalone/src/questionnaire/QuestionnaireTableElement.js
+++ flashcard-react-standalone/src/questionnaire/QuestionnaireTableElement.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import QuestionnaireShowDialog from './QuestionnaireShowDialog'
 
 const textStyle = {
-  verticalAlign: 'middle'
+    verticalAlign: 'middle'
 };
 
-const QuestionnaireTableElement = ( {questionnaire } ) => (
-  <tr>
-    <td colSpan="1" style={textStyle}>
-      {questionnaire.id}
-    </td>
-    <td colSpan="3" style={textStyle}>
-      {questionnaire.title}
-    </td>
-    <td colSpan="10" style={textStyle}>
-      {questionnaire.description}
-    </td>
-  </tr>
+const QuestionnaireTableElement = ({questionnaire}) => (
+    <tr>
+        <td colSpan="1" style={textStyle}>
+            {questionnaire.id}
+        </td>
+        <td colSpan="3" style={textStyle}>
+            {questionnaire.title}
+        </td>
+        <td colSpan="10" style={textStyle}>
+            {questionnaire.description}
+        </td>
+        <td colSpan="1" style={textStyle}>
+            <QuestionnaireShowDialog questionnaire={questionnaire}/>
+        </td>
+    </tr>
 )
 
 export default QuestionnaireTableElement;
