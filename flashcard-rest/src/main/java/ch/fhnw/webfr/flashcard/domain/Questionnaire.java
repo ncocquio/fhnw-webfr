1592d5147f9ab86027141217d4c56160c5516fc8
Nicola Cocquio <nicola.cocquio@students.fhnw.ch>
2018-11-19 17:23:17 +0100

    AB19, AB20 and UB9

---
+++ flashcard-rest/src/main/java/ch/fhnw/webfr/flashcard/domain/Questionnaire.java
@@ -0,0 +1,43 @@
+package ch.fhnw.webfr.flashcard.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.Size;
+
+@Document(collection="questionnaires")
+public class Questionnaire {
+    @Id
+    private String id;
+
+	@Size(min = 2, max = 30)
+    private String  title;
+
+    @Size(min = 10, max = 50)
+    private String description;
+    
+    public void setId(String id) {
+		this.id = id;
+	}
+    
+    public String getId() {
+		return id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+    
+    public String getTitle() {
+		return title;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+}
---
+++ flashcard-rest/src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
@@ -0,0 +1,72 @@
+package ch.fhnw.webfr.flashcard.web;
+
+import ch.fhnw.webfr.flashcard.domain.Questionnaire;
+import ch.fhnw.webfr.flashcard.persistence.QuestionnaireRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/questionnaires")
+public class QuestionnaireController {
+    @Autowired
+    private QuestionnaireRepository questionnaireRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Questionnaire>> findAll() {
+        return new ResponseEntity<>(questionnaireRepository.findAllByOrderByIdAsc(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Questionnaire> findQuestionnaireById(@PathVariable String id) {
+        Optional<Questionnaire> maybeQuestionnaire = questionnaireRepository.findById(id);
+        if (maybeQuestionnaire.isPresent()) {
+            return new ResponseEntity<>(maybeQuestionnaire.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Questionnaire> createQuestionnaire(@Valid @RequestBody Questionnaire questionnaire, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(questionnaire, HttpStatus.PRECONDITION_FAILED);
+        }
+
+        Questionnaire savedQuestionnaire = questionnaireRepository.insert(questionnaire);
+
+        return new ResponseEntity<>(savedQuestionnaire, HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Questionnaire> updateQuestionnaireById(@PathVariable String id, @Valid @RequestBody Questionnaire questionnaire, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(questionnaire, HttpStatus.PRECONDITION_FAILED);
+        }
+
+        Optional<Questionnaire> maybeQuestionnaire = questionnaireRepository.findById(id);
+        if (maybeQuestionnaire.isPresent()) {
+            Questionnaire savedQuestionnaire = questionnaireRepository.save(questionnaire);
+            return new ResponseEntity<>(savedQuestionnaire, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Questionnaire> deleteQuestionnaireById(@PathVariable String id) {
+        Optional<Questionnaire> maybeQuestionnaire = questionnaireRepository.findById(id);
+        if (maybeQuestionnaire.isPresent()) {
+            questionnaireRepository.delete(maybeQuestionnaire.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
